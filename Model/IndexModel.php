f79c4b3ccb655df03c8d2ac18271882355f1183c
Stef Robbe <627468@student.inholland.nl>
2019-12-17 18:33:04 +0100

    device transfer

---
+++ Model/IndexModel.php
@@ -0,0 +1,7 @@
+<?php
+require_once("Autoloader.php");
+class IndexModel
+{
+	function __construct() {	}
+}
+?>
---
+++ View/IndexView.php
@@ -0,0 +1,36 @@
+<?php 
+require_once("Autoloader.php");
+
+class IndexView
+{
+	private $IndexController;
+	private $IndexModel;
+
+	public function __construct($indexController, $indexModel)
+	{
+		$this->IndexController = $indexController;
+		$this->IndexModel = $indexModel;
+	}
+
+	//output to html
+	public function output(){
+		$page = "";
+		$page .= $this->Header();
+		$page .= $this->Body();
+		$page .= $this->Footer();
+		return $page;
+	}
+
+	private function Header() {
+		return "";
+	}
+
+	private function Body() {
+		return "";
+	}
+
+	private function Footer() {
+		return "";
+	}
+}
+?>
